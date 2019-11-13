4d62ffd27339fb18242c79f085586eb23592c292
konglp1997 <konglp1997@163.com>
2019-11-13 16:23:16 +0800

    基本架构完成

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    // 只是 px to rem 配置
+    // 同时 需要一个插件 amfe
+    'postcss-pxtorem': {
+      // 换算rem基准值 标准设备iphone6 宽度375
+      // 但是设计稿是750px 缩小一倍
+      rootValue: 37.5,
+      propList: ['*']
+    }
   }
+
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入设置基准值
+import 'amfe-flexible'
+
+// 导入vant组件库
+import vant from 'vant'
+import 'vant/lib/index.css'
+
+// 需要在vant样式之后引入全局样式
+import '@/styles/index.less'
+Vue.use(vant)
+
 Vue.config.productionTip = false
 
 new Vue({
